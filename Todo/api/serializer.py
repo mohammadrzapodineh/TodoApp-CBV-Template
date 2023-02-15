81e19b575eb5eec5e6b6dcc14c4cb1aa131310f5
mohammad reza poodineh <mohammadrzapodineh@gmail.com>
2023-02-15 09:47:13 -0800

    Create Todo List And Todo Detail Api

---
+++ Todo/api/serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from Todo.models import Todo
+
+# class TodoSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=110)
+#     is_done = serializers.BooleanField(default=False)
+
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    '''
+    This Serializer Work With ModelSeializer !
+    '''
+    class Meta:
+        model = Todo
+        fields = ('title', 'is_done', 'user')
--- Todo/api/v1/views.py
+++ Todo/api/v1/views.py
@@ -1,11 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from Todo.api.serializer import TodoSerializer
+from Todo.models import Todo
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def todo_list(request):
-    return Response({'status': 'ok'})
+    if request.method == 'GET':
+        object_list = Todo.objects.all()
+        serializer = TodoSerializer(object_list, many=True) 
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view()
 def todo_detail(request, pk):
-    return Response({'status': '200'})
+    todo_object = get_object_or_404(Todo, id=pk)
+    serializer = TodoSerializer(todo_object)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
